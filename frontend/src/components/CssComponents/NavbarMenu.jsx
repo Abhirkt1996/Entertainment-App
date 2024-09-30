9c8945069d5320a123e38428c609e2b9242d47a8
Abhishek Kumar <your-abhiabhishekrkt1996@gmail.com>
2024-09-30 21:51:46 +0530

    Add frontend as regular folder

---
+++ frontend/src/components/CssComponents/NavbarMenu.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AppsIcon from '@mui/icons-material/Apps';
+import { Button, Box } from '@mui/material';
+import userLogo from '../../assets/userLogo.png';
+
+export default function NavbarMenu() {
+    const ITEM_HEIGHT = 48;
+    const [anchorEl, setAnchorEl] = useState(null);
+
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const items = [
+        {
+            name: "Code",
+            lastName: "Book",
+            image: userLogo,
+            link: 'https://github.com/Abhirkt1996/FullStackNotes'
+        },
+        {
+            name: 'Abhishek',
+            lastName: "Kumar",
+            image: userLogo,
+            link: 'https://my-portfolio-ashen-eta.vercel.app/'
+        },
+        {
+            name: 'Almabetter',
+            lastName: 'CryptoDashboard',
+            image: userLogo,
+            link: 'almabetter-crypto-dashboard.vercel.app'
+        },
+    ];
+
+    return (
+        <Box>
+            <IconButton
+                sx={{
+                    color: '#5A698F',
+                    backgroundColor: '#161D2F',
+                    '&:hover': {
+                        color: '#06b6d4',
+                    },
+                }}
+                aria-label='more'
+                aria-controls='long_menu'
+                aria-expanded='true'
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <AppsIcon sx={{ fontSize: '36px' }} />
+            </IconButton>
+            <Menu
+                id='long_menu'
+                anchorEl={anchorEl}
+                MenuListProps={{ 'aria-labelledby': 'long-button' }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        width: '32ch',
+                        maxHeight: ITEM_HEIGHT * 6.5,
+                        backgroundColor: '#000000',
+                    },
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(2, 1fr)',
+                        gap: '24px',
+                        padding: '16px',
+                    }}
+                >
+                    {items.map((item, index) => (
+                        <MenuItem
+                            key={index}
+                            onClick={handleClose}
+                            sx={{ padding: 0 }}
+                        >
+                            <a
+                                href={item.link}
+
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'inherit',
+                                    width: '100%',
+                                }}
+                            >
+                                <Button
+                                    variant={'text'}
+                                    sx={{
+                                        width: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        '&:hover': {
+                                            color: '#000000',
+                                            backgroundColor: '#06b6d4',
+                                        },
+                                    }}
+                                >
+                                    <img
+                                        src={item.image}
+                                        alt={item.name}
+                                        style={{ width: '24px', height: '24px', marginRight: '8px' }}
+                                    />
+                                    <span
+                                        style={{
+                                            maxWidth: '80px',
+                                            textAlign: 'center',
+                                            textTransform: 'capitalize'
+                                        }}
+                                    >
+                                        {item.name}
+                                    </span>
+                                    <span
+                                        style={{
+                                            maxWidth: '80px',
+                                            textAlign: 'center',
+                                            textTransform: 'capitalize'
+                                        }}
+                                    >
+                                        {item.lastName}
+                                    </span>
+                                </Button>
+                            </a>
+                        </MenuItem>
+                    ))}
+                </Box>
+            </Menu >
+        </Box >
+    );
+}

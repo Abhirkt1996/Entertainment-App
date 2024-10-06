22d52e25e7f261cb8b2f0c66e2479b65af9077f9
Abhishek Kumar <your-abhiabhishekrkt1996@gmail.com>
2024-10-06 09:19:56 +0530

    update code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,40 +1,43 @@
-// importing installed packages 
+// Importing installed packages
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const cors = require('cors');
 
-// importing routes 
-const { bookmarkRouter } = require('./routes/bookmark.routes.js')
-const { mediaRouter } = require('./routes/media.routes.js')
-const { userRouter } = require('./routes/user.routes.js')
+// Importing routes
+const { bookmarkRouter } = require('./routes/bookmark.routes.js');
+const { mediaRouter } = require('./routes/media.routes.js');
+const { userRouter } = require('./routes/user.routes.js');
 
-// app instances 
+// App instances
 const app = express();
 
-
-// Middleware 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS configuration
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL, // Ensure this is correct
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+    credentials: true, // Allows cookies and authorization headers
+}));
 
+// Pre-flight requests
+app.options('*', cors()); // Enable pre-flight for all routes
 
-// routes 
+// Routes
 app.use("/api", bookmarkRouter);
 app.use("/api", mediaRouter);
 app.use("/api", userRouter);
 
-// home route 
+// Home route
 app.get('/', (req, res) => {
     res.json({
-        message: "Welcome to home route "
-    })
-})
+        message: "Welcome to home route"
+    });
+});
 
-// exporting 
-module.exports = { app }
+// Exporting
+module.exports = { app };
 
